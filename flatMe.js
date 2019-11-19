76605268630e25445cbdb2aef00363e902812b20
Simone DELLA MARCA <simone.dellamarca@altran.it>
2019-11-19 17:21:35 +0100

    Added no recursive version

--- flatMe.js
+++ flatMe.js
@@ -1,13 +1,34 @@
 
 //A brand new function to flat a multidimensional array
 
-const flatMe = arrayToFlat => {
-    let flattenArray = [];
+const flatMe = (arrayToFlat,flattenArray) => {
+    flattenArray = !flattenArray ? []: flattenArray;
     arrayToFlat.forEach((int)=>{
         Array.isArray(int) ? flattenArray.push(...flatMe(int)) : flattenArray.push(int);
     });
     return flattenArray;
 };
+
+const flatMeNoRecursion = function flatten(arr) {
+    var result = [];
+    var stack = arr;
+    var first;
+  
+    while (stack.length > 0) {
+      first = stack[0];
+  
+      if (Array.isArray(first)) {
+        // Replace the nested array with its items
+        Array.prototype.splice.apply(stack, [0, 1].concat(first));
+      } else {
+        result.push(first);
+        // Delete the first item
+        stack.splice(0, 1);
+      }
+    }
+  
+    return result;
+  };
 
 module.exports = flatMe;
 
